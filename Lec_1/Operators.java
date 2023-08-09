3aab9a2a1d8602ffa77a52c54d24a25af9381314
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-09 15:10:30 +0530

    Basics

---
+++ Lec_1/Operators.java
@@ -0,0 +1,16 @@
+package Lec_1;
+
+public class Operators {
+    public static void main(String[] args) {
+        int Y=29;
+        Y+=3;
+
+        int divide=3/8;
+        int mod=3%8;
+        System.out.println(divide);
+        System.out.println(mod);
+
+        System.out.println(3>=3);
+        System.out.println(3>3 || 2==2 || 3<3);
+    }
+}
---
+++ Lec_1/Variables.java
@@ -0,0 +1,17 @@
+package Lec_1;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print("Hello world");
+        System.out.println("Hello world");
+
+        int x=100;
+        x=200;
+//        int 2c=2000;
+        int ab2=999;
+        int _ab=23;
+//        int h u=22;
+
+    }
+}
