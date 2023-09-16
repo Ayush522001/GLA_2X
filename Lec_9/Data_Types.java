70b40a43c6395670c4d3bc96c778dadbe18adafd
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-09-16 10:26:29 +0530

    Data Types

---
+++ Lec_9/Data_Types.java
@@ -0,0 +1,16 @@
+package Lec_9;
+
+public class Data_Types {
+    public static void main(String[] args) {
+        byte a=12;
+        short b=22;
+        int c=222;
+        long d=2300;
+
+        float e=2.23f;
+        double g=2.889;
+
+        char ch='*';
+        boolean h=true;
+    }
+}
